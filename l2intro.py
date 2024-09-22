29af321d478d61e92f8f1399fa7df9519244542d
Arkar Hein <arkerhein44@gmail.com>
2024-09-22 23:11:55 +0630

    "22.9.2024 lesson"

--- l2intro.py
+++ l2intro.py
@@ -1,4 +1,71 @@
 print("Hello Sir")
 
+# Arithmetic Operators
+# 1. Addition (+)
+# 2. Subtraction (-)
+# 3. Multiplication (*)
+# 4. Division | Float Division(/)
+# 5. Floor Division (//)
+# 6. Modulus (%)
+# 7. Exponentiation (**)
 
-# 15PT
+print(20 + 5)
+print(20 - 5)
+print(20 * 5)
+print(20 / 5)
+print(20 // 5)
+print(18 / 5)
+print(18 // 5)
+print(20 % 5)
+print(2 ** 10)
+
+# => Operators Precedence
+# Parentheses
+# Exponent
+# Multiplication Division (first in, first cal)
+# Addition Subtraction (first in,first cal)
+
+print((2 + 5) * 4 / 10 - (10 % 2))  # 2.8
+# 7*4/10-0
+# 28/10
+# 2.8
+
+firstname = "aung kyaw"
+last_name = "kyaw"
+print(firstname)
+print(last_name)
+print(firstname, last_name)
+
+x = 10
+y = 20
+p = '30'
+q = '40'
+
+sum1 = x + y
+sum2 = p + q
+# sum3 = x + q error
+
+print(sum1)
+print(sum2)
+# print(sum3) error can't + for int and str
+
+num1 = 100
+num2 = 200.125
+name = "su su"
+hascar = True  # first letter must be capital for bool
+
+print(num1, num2)  # 100 200.125
+print(type(num1))  # <class 'int'>
+print(type(num2))  # <class 'float'>
+print(type(name))  # <class 'str'>
+print(type(hascar))  # <class 'bool'>
+
+colors = ["red","green","blue"]
+print(colors)
+print(type(colors))  # <class 'list'>
+
+ages = [20,25,30,35]
+print(ages)
+
+# 22AR
+
